6cdb2800584306fa7e683ec4e7392a192b2569ea
adamkul097 <adamkul097@outlook.com>
2019-04-30 08:27:40 +0200

    printer przeciazona metoda

--- CSharpZaliczenie/Printer.cs
+++ CSharpZaliczenie/Printer.cs
@@ -8,6 +8,18 @@ namespace CSharpZaliczenie
 {
     class Printer : IDrukujPracownikow
     {
+        public void PrintAllData(Pracownik pracownik)
+        {
+            Console.WriteLine("Imie: {0}", pracownik.Imie);
+            Console.WriteLine("Nazwisko: {0}", pracownik.Nazwisko);
+            Console.WriteLine("Wiek: {0}", pracownik.Wiek);
+            Console.WriteLine("Stanowisko: {0}", pracownik.Stanowisko);
+            Console.WriteLine("Pensja miesieczna: {0}", pracownik.PensjaMiesieczna);
+            Console.WriteLine("Data zatrudnienia: {0}", pracownik.DataZatrudnienia);
+            Console.WriteLine("Ilosc dni urlopu: {0}", pracownik.IloscDniUrlopu);
+            Console.WriteLine();
+        }
+
         public void PrintAllData(IList<Pracownik> pracownicy)
         {
             int i = 1;
