942fb4e51c5859695532a80ab04695927e5feebd
adam.kulasiak <adam.kulasiak@gmail.com>
2019-05-03 15:02:32 +0200

    referencja

---
+++ FnRef/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FnRef
+{
+    class Program
+    {
+        static void Inkrementacja(int l)
+        {
+            l++;
+        }
+
+        static void InkrementacjaRef(ref int l)
+        {
+            l++;
+        }
+
+        static void Main(string[] args)
+        {
+            int liczba = 0;
+            Console.WriteLine("Liczba: {0}", liczba);
+            Inkrementacja(liczba);
+            Console.WriteLine("Inkrementacja przez wartosc: {0}", liczba);
+            InkrementacjaRef(ref liczba);
+            Console.WriteLine("Inkrementacja przez referencje: {0}", liczba);
+
+            Console.ReadKey();
+        }
+    }
+}
